5aa4b7d8e26c1060cd43a1cc81cc93e090275921
Satyo Wicaksana <satyowicaksana@gmail.com>
2019-12-30 23:07:45 +0700

    complete the lists

--- src/Portfolios.jsx
+++ src/Portfolios.jsx
@@ -29,7 +29,77 @@ const data = [
       </ul>
     ),
     link: 'http://checkit.satyowicaksana.com'
-  }
+  },
+  {
+    name: 'omniverse',
+    title: 'Omniverse',
+    description: 'E-commerce web',
+    content: (
+      <ul>
+        <li>Vue.js</li>
+        <li>Vuetify</li>
+        <li>Express</li>
+        <li>Mongoose</li>
+      </ul>
+    ),
+    link: 'http://omniverse.satyowicaksana.online'
+  },
+  {
+    name: 'fakeoverflow',
+    title: 'Fake Overflow',
+    description: 'Stack Overflow clone web',
+    content: (
+      <ul>
+        <li>Vue.js</li>
+        <li>Buefy</li>
+        <li>Express</li>
+        <li>Mongoose</li>
+      </ul>
+    ),
+    link: 'http://fakeoverflow.satyowicaksana.online'
+  },
+  {
+    name: 'peregrine',
+    title: 'Peregrine',
+    description: 'Kanban (task manager) web',
+    content: (
+      <ul>
+        <li>Vue.js</li>
+        <li>Buefy</li>
+        <li>Express</li>
+        <li>Firebase</li>
+      </ul>
+    ),
+    link: 'http://peregrine.satyowicaksana.online'
+  },
+  {
+    name: 'shield',
+    title: 'S.H.I.E.L.D. Database',
+    description: 'Superhero list (react exercise) web',
+    content: (
+      <ul>
+        <li>React.js</li>
+        <li>Material UI</li>
+        <li>Express</li>
+        <li>Firebase</li>
+      </ul>
+    ),
+    link: 'https://shield-database.firebaseapp.com'
+  },
+  {
+    name: 'vaultron',
+    title: 'Vaultron',
+    description: 'Password Manager web',
+    content: (
+      <ul>
+        <li>React.js</li>
+        <li>React Semantic UI</li>
+        <li>Express</li>
+        <li>Firebase</li>
+      </ul>
+    ),
+    link: 'https://vaultron-pass.firebaseapp.com'
+  },
 ];
 
 const IconText = ({ type, text }) => (
